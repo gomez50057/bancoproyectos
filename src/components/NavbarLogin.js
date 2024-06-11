5a86823504d28422940546ea7be9d9983fe54673
gomez50057 <gomez.50057@gmail.com>
2024-06-11 11:17:12 -0600

    login

--- src/components/NavbarLogin.js
+++ src/components/NavbarLogin.js
@@ -9,9 +9,11 @@ const NavbarLogin = () => {
 
     return (
         <nav className="NavbarLogin">
-            <img src={`${imgBasePath}head.png`} alt="img_representativa" />
             <ul>
-                <li><Link to="/" className=""> Banco de Proyectos </Link></li>
+                <div className="NavbarLogin_img">
+                    <li><Link to="/" className=""> Banco de Proyectos </Link></li>
+                    <img src={`${imgBasePath}head.png`} alt="img_representativa" />
+                </div>
                 <div className="NavbarLogin_inicio">
                     <li><Link to="/login" className=""> Acceder </Link></li>
                     <li><Link to="/" className=""> Registrate </Link></li>
