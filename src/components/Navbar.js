c3fb16454d133a0c2df1be006a31a19273689788
gomez50057 <gomez.50057@gmail.com>
2024-06-17 11:29:08 -0600

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
-// const imgBasePath = "https://bibliotecadigitaluplaph.hidalgo.gob.mx/img_banco/";
+
 const imgBasePath = "img/";
 
 const Navbar = () => {
@@ -19,14 +19,24 @@ const Navbar = () => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, [scrollPosition]);
 
+    const handleScroll = (event, targetId) => {
+        event.preventDefault();
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    };
+
     return (
         <nav className={`navbar ${visible ? 'active' : 'hidden'} ${scrollPosition > 100 ? 'scrolled' : ''}`}>
             <ul>
-            <img src={`${imgBasePath}Logotipo.png`} alt="img_representativa" />
-
-                <li><a href="#about">Sobre el Proyecto</a></li>
-                <li><a href="#projects">Tablero de Proyectos</a></li>
-                <li><a href="#howitWorks">Cómo Funciona</a></li>
+                <img src={`${imgBasePath}Logotipo.png`} alt="img_representativa" />
+                <li><a href="#about" onClick={(e) => handleScroll(e, 'about')}>Sobre el Proyecto</a></li>
+                <li><a href="#projects" onClick={(e) => handleScroll(e, 'projects')}>Tablero de Proyectos</a></li>
+                <li><a href="#howitWorks" onClick={(e) => handleScroll(e, 'howitWorks')}>Cómo Funciona</a></li>
                 {/* <li><a href="#map">Mapa Interactivo</a></li> */}
                 <li><Link to="/login" className="button_acceder">Acceder</Link></li>
             </ul>
