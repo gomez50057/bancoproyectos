c7ab1546a807891706cf08f684ebcd22dbd81e60
gomez50057 <gomez.50057@gmail.com>
2024-06-20 15:36:32 -0600

    table

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,29 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import MUIDataTable from "mui-datatables";
+import MUIDataTable from 'mui-datatables';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import axios from 'axios';
 
 const TableComponent = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    // Función para obtener los proyectos desde la API
     const fetchProjects = async () => {
       try {
-        const response = await axios.get('/ver-proyectos-tabla');
-        setProjects(response.data);
+        const response = await axios.get('/ver-proyectos-tabla/');
+        const data = response.data.map(project => [
+          project.project_name,
+          project.descripcion,
+          project.tipo_proyecto,
+          project.municipio,
+          project.beneficiarios
+        ]);
+        setProjects(data);
       } catch (error) {
         console.error('Error fetching projects:', error);
       }
     };
-
-    // Llamada a la función de obtención de proyectos
     fetchProjects();
-  }, []); // Se ejecuta solo una vez al cargar el componente
+  }, []);
 
-  const columns = ["Nombre del Proyecto", "Descripción", "Tipo de Proyecto", "Municipio", "Beneficiarios"];
+  const columns = [
+    { name: "Nombre del Proyecto", options: { setCellProps: () => ({ style: { fontWeight: 700, textAlign: 'left' } }) } },
+    { name: "Descripción", options: { setCellProps: () => ({ style: { textAlign: 'justify' } }) } },
+    "Tipo de Proyecto",
+    { name: "Municipio", options: { setCellProps: () => ({ style: { textAlign: 'center' } }) } },
+    { name: "Beneficiarios", options: { setCellProps: () => ({ style: { textAlign: 'center' } }) } }
+  ];
 
-   const options = {
+  const options = {
     selectableRows: false,
+    setRowProps: (row, dataIndex) => ({
+      style: {
+        backgroundColor: dataIndex % 2 === 0 ? 'rgba(255, 255, 255, 0.8)' : 'rgba(240, 240, 240, 0.8)',
+        backdropFilter: 'blur(5px)',
+        margin: '5px 0',
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+        transition: 'all 0.3s ease',
+        '&:hover': {
+          backgroundColor: dataIndex % 2 === 0 ? 'rgba(230, 230, 230, 0.8)' : 'rgba(255, 255, 255, 0.8)',
+          boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)',
+        },
+      },
+    }),
     textLabels: {
       body: {
         noMatch: "No se encontraron registros",
@@ -59,15 +84,75 @@ const TableComponent = () => {
     }
   };
 
+  const getMuiTheme = () =>
+    createTheme({
+      components: {
+        MUIDataTableBodyCell: {
+          styleOverrides: {
+            root: {
+              padding: '8px 16px',
+              '&:nth-of-type(1)': {
+                fontWeight: 600,
+                textAlign: 'left',
+              },
+              
+            },
+          },
+        },
+        MUIDataTableHeadCell: {
+          styleOverrides: {
+            root: {
+              fontWeight: 600,
+              backgroundColor: '#f2f2f2',
+              textAlign: 'center',
+            },
+          },
+        },
+        MUIDataTableToolbar: {
+          styleOverrides: {
+            root: {
+              marginBottom: '10px',
+            },
+          },
+        },
+        MUIDataTable: {
+          styleOverrides: {
+            root: {
+              boxShadow: 'none',
+            },
+            paper: {
+              boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+              margin: '20px',
+              backgroundColor: 'rgba(255, 255, 255, 0.7)',
+              backdropFilter: 'blur(5px)',
+            },
+          },
+        },
+        MuiTableRow: {
+          styleOverrides: {
+            root: {
+              '&:hover': {
+                backgroundColor: 'rgba(230, 230, 230) !important',
+                boxShadow: '0 4px 10px rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        },
+      },
+    });
+
   return (
-    <div className="table_grid">
-      <MUIDataTable
-        title={"Lista de proyectos"}
-        data={projects}
-        columns={columns}
-        options={options}
-      />
-    </div>
+    <ThemeProvider theme={getMuiTheme()}>
+      <CssBaseline />
+      <div className="table_grid">
+        <MUIDataTable
+          title={"Lista de proyectos"}
+          data={projects}
+          columns={columns}
+          options={options}
+        />
+      </div>
+    </ThemeProvider>
   );
 }
 
