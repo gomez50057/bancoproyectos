fb2b9171708ff18aacd5f6e645823e9e67758db6
gomez50057 <gomez.50057@gmail.com>
2024-07-15 13:17:59 -0600

    ClientProjects

---
+++ src/pages/Client/ClientProjects.js
@@ -0,0 +1,172 @@
+// Archivo: src/pages/Client/ClientProjects.js
+import React, { useState, useEffect } from 'react';
+import MUIDataTable from 'mui-datatables';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Typography } from '@mui/material';
+import axios from 'axios';
+
+const ClientProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get('/ver-proyectos-tabla/');
+        const filteredData = response.data.filter(project => {
+          const estatus = project.estatus;
+          return estatus === 'Atendido' || estatus === 'En Proceso';
+        });
+        const data = filteredData.map(project => [
+          project.project_id,
+          project.project_name,
+          project.estatus,
+          project.porcentaje_avance
+        ]);
+        setProjects(data);
+      } catch (error) {
+        console.error('Error fetching projects:', error);
+      }
+    };
+    fetchProjects();
+  }, []);
+
+  const columns = [
+    { name: "ID del Proyecto", options: { setCellProps: () => ({ style: { fontWeight: 700, textAlign: 'left' } }) } },
+    { name: "Nombre del Proyecto", options: { setCellProps: () => ({ style: { textAlign: 'left' } }) } },
+    { name: "Estatus", options: { setCellProps: () => ({ style: { textAlign: 'center' } }) } },
+    { name: "Porcentaje de Avance", options: { setCellProps: () => ({ style: { textAlign: 'center' } }) } }
+  ];
+
+  const options = {
+    selectableRows: 'none',
+    download: false,
+    print: false,
+    setRowProps: (row, dataIndex) => ({
+      style: {
+        backgroundColor: dataIndex % 2 === 0 ? 'rgba(255, 255, 255, 0.8)' : 'rgba(240, 240, 240, 0.8)',
+        backdropFilter: 'blur(5px)',
+        margin: '5px 0',
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+        transition: 'all 0.3s ease',
+        '&:hover': {
+          backgroundColor: dataIndex % 2 === 0 ? 'rgba(230, 230, 230, 0.8)' : 'rgba(255, 255, 255, 0.8)',
+          boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)',
+        },
+      },
+    }),
+    textLabels: {
+      body: {
+        noMatch: "No se encontraron registros",
+        toolTip: "Ordenar",
+      },
+      pagination: {
+        next: "Siguiente",
+        previous: "Anterior",
+        rowsPerPage: "Filas por página:",
+        displayRows: "de",
+      },
+      toolbar: {
+        search: "Buscar",
+        viewColumns: "Ver columnas",
+        filterTable: "Filtrar tabla",
+      },
+      filter: {
+        all: "Todos",
+        title: "FILTROS",
+        reset: "REINICIAR",
+      },
+      viewColumns: {
+        title: "Mostrar columnas",
+        titleAria: "Mostrar/Ocultar columnas",
+      },
+      selectedRows: {
+        text: "fila(s) seleccionada(s)",
+        delete: "Eliminar",
+        deleteAria: "Eliminar filas seleccionadas",
+      },
+    }
+  };
+
+  const getMuiTheme = () =>
+    createTheme({
+      components: {
+        MUIDataTableBodyCell: {
+          styleOverrides: {
+            root: {
+              padding: '8px 16px',
+              '&:nth-of-type(1)': {
+                fontWeight: 600,
+                textAlign: 'left',
+              },
+            },
+          },
+        },
+        MUIDataTableHeadCell: {
+          styleOverrides: {
+            root: {
+              fontWeight: 600,
+              backgroundColor: 'none',
+              textAlign: 'center',
+            },
+          },
+        },
+        MUIDataTableToolbar: {
+          styleOverrides: {
+            root: {
+              marginBottom: '10px',
+            },
+          },
+        },
+        MUIDataTable: {
+          styleOverrides: {
+            root: {
+              boxShadow: 'none',
+            },
+            paper: {
+              boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+              margin: '20px',
+              backgroundColor: 'rgba(255, 255, 255, 0.7)',
+              backdropFilter: 'blur(5px)',
+            },
+          },
+        },
+        MuiTableRow: {
+          styleOverrides: {
+            root: {
+              '&:hover': {
+                backgroundColor: 'rgba(230, 230, 230) !important',
+                boxShadow: '0 4px 10px rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        },
+        MuiTypography: {
+          styleOverrides: {
+            h3: {
+              fontWeight: 600,
+              fontSize: '2.25rem',
+              color: '#DEC9A3',
+              fontFamily: "Montserrat",
+              padding: '10px'
+            },
+          },
+        },
+      },
+    });
+
+  return (
+    <ThemeProvider theme={getMuiTheme()}>
+      <CssBaseline />
+      <div className="table_grid_pro">
+        <MUIDataTable
+          title={<Typography variant="h3">Proyectos Registrados</Typography>}
+          data={projects}
+          columns={columns}
+          options={options}
+        />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default ClientProjects;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -7,6 +7,7 @@ import FormDependencia from '../pages/Client/FormDependencia';
 import TableComponent from '../pages/FrontPage/TableComponent';
 import CRUDTable from '../pages/Responsible/CRUDTable';
 import ClientPanel from '../pages/Client/ClientPanel';
+import ClientProjects from '../pages/Client/ClientProjects'; // Importa el nuevo componente
 import PrivateRoute from './PrivateRoute'; 
 import Navbar from '../components/Navbar';
 import NavbarLogin from '../components/NavbarLogin';
@@ -28,11 +29,12 @@ const LoginLayout = ({ children }) => (
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<MainLayout><MainContent /></MainLayout>} /> 
+            <Route path="/" element={<MainLayout><MainContent /></MainLayout>} />
             <Route path="/login" element={<LoginLayout><Login /></LoginLayout>} />
             <Route path="/table" element={<MainLayout><TableComponent /></MainLayout>} />
             <Route path="/dependencia/*" element={<PrivateRoute allowedGroups={['cliente']}><MainLayout><FormDependencia /></MainLayout></PrivateRoute>} />
             <Route path="/panel-usuario/*" element={<PrivateRoute allowedGroups={['cliente']}><MainLayout><ClientPanel /></MainLayout></PrivateRoute>} />
+            <Route path="/consulta" element={<PrivateRoute allowedGroups={['cliente']}><MainLayout><ClientProjects /></MainLayout></PrivateRoute>} />
             <Route path="/crud" element={<PrivateRoute allowedGroups={['responsable']}><MainLayout><CRUDTable /></MainLayout></PrivateRoute>} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
