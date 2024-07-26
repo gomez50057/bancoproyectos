a230b38f99264ca2d4a2502cc6a238e6db7ce5e3
gomez50057 <gomez.50057@gmail.com>
2024-07-26 13:42:42 -0600

    react pdf

--- src/pages/Responsible/ProjectReport.js
+++ src/pages/Responsible/ProjectReport.js
@@ -1,4 +1,3 @@
-// ProjectReport.js
 import React from 'react';
 import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 
@@ -6,7 +5,8 @@ import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/render
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
-    padding: 30,
+    backgroundColor: '#FFFFFF',
+    padding: 0, // Asegurarse de que no haya padding
   },
   header: {
     textAlign: 'center',
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
   },
   section: {
     marginBottom: 10,
+    paddingHorizontal: 30,
   },
   label: {
     fontSize: 12,
@@ -31,13 +32,40 @@ const styles = StyleSheet.create({
     height: 50,
     marginBottom: 20,
   },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+    width: '100%',
+  },
+  rectangle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 232,
+    height: 551,
+    borderTopWidth: 0, // Sin borde superior
+    borderLeftWidth: 0, // Sin borde izquierdo
+    borderBottomWidth: 2, // Borde inferior
+    borderRightWidth: 2, // Borde derecho
+    borderColor: '#691B32', // Color del borde
+    borderStyle: 'solid',
+    borderBottomRightRadius: 40, // Radio del borde en la esquina inferior derecha
+    shadowColor: '#000',
+    shadowOffset: { width: 10, height: 10 }, // Incrementar sombra para efecto 3D
+    shadowOpacity: 0.5, // Incrementar opacidad de la sombra
+    shadowRadius: 15, // Incrementar radio de la sombra
+    elevation: 10,
+  },
 });
 
 const ProjectReport = ({ project }) => (
   <Document>
-    <Page size="A4" style={styles.page}>
+    <Page size="A4" style={styles.page} orientation="landscape">
+      <View style={styles.rectangle}></View>
       <View style={styles.header}>
-        <Image src="https://buenaspracticas.hidalgo.gob.mx/img/Logotipo.png" style={styles.image} />
         <Text style={styles.title}>Ficha del Proyecto</Text>
       </View>
       <View style={styles.section}>
@@ -57,6 +85,7 @@ const ProjectReport = ({ project }) => (
         <Text style={styles.value}>{project.objetivos}</Text>
       </View>
       {/* Añade más campos según sea necesario */}
+      <Image src="https://bibliotecadigitaluplaph.hidalgo.gob.mx/img_banco/pdf/footer_pdf.png" style={styles.footer} />
     </Page>
   </Document>
 );
