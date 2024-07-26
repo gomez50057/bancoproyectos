69a27a2dd074e83bed44c9f2d84289de061949da
gomez50057 <gomez.50057@gmail.com>
2024-07-26 15:56:04 -0600

    react pdf

--- src/pages/Responsible/ProjectReport.js
+++ src/pages/Responsible/ProjectReport.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
+import footerImage from '../../assets/images/pdf/footer_pdf.png';
 
 // Estilos para el PDF
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
     backgroundColor: '#FFFFFF',
-    padding: 0, // Asegurarse de que no haya padding
+    padding: 0,
   },
   header: {
     textAlign: 'center',
@@ -27,11 +28,6 @@ const styles = StyleSheet.create({
   value: {
     fontSize: 12,
   },
-  image: {
-    width: 200,
-    height: 50,
-    marginBottom: 20,
-  },
   footer: {
     position: 'absolute',
     bottom: 0,
@@ -46,17 +42,17 @@ const styles = StyleSheet.create({
     left: 0,
     width: 232,
     height: 551,
-    borderTopWidth: 0, // Sin borde superior
-    borderLeftWidth: 0, // Sin borde izquierdo
-    borderBottomWidth: 2, // Borde inferior
-    borderRightWidth: 2, // Borde derecho
-    borderColor: '#691B32', // Color del borde
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderBottomWidth: 2,
+    borderRightWidth: 2,
+    borderColor: '#691B32',
     borderStyle: 'solid',
-    borderBottomRightRadius: 40, // Radio del borde en la esquina inferior derecha
+    borderBottomRightRadius: 40,
     shadowColor: '#000',
-    shadowOffset: { width: 10, height: 10 }, // Incrementar sombra para efecto 3D
-    shadowOpacity: 0.5, // Incrementar opacidad de la sombra
-    shadowRadius: 15, // Incrementar radio de la sombra
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: 0.5,
+    shadowRadius: 15,
     elevation: 10,
   },
 });
@@ -85,7 +81,7 @@ const ProjectReport = ({ project }) => (
         <Text style={styles.value}>{project.objetivos}</Text>
       </View>
       {/* Añade más campos según sea necesario */}
-      <Image src="https://bibliotecadigitaluplaph.hidalgo.gob.mx/img_banco/pdf/footer_pdf.png" style={styles.footer} />
+      <Image src={footerImage} style={styles.footer} />
     </Page>
   </Document>
 );
