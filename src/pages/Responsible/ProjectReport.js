36f398a9d739fd86bfae709c2ab7813e9cba6a8c
gomez50057 <gomez.50057@gmail.com>
2024-07-28 00:04:53 -0600

    footer ajuste

--- src/pages/Responsible/ProjectReport.js
+++ src/pages/Responsible/ProjectReport.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import footerImage from '../../assets/images/pdf/footer_pdf.png';
+import proyectoImage from '../../assets/images/pdf/foto_proyecto.png';
 
 // Estilos para el PDF
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row', // Cambiado a 'row' para alinear elementos horizontalmente
+    flexDirection: 'row',
     backgroundColor: '#FFFFFF',
     padding: 0,
     margin: 0,
@@ -15,14 +16,13 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 16,
     marginBottom: 20,
   },
-  sectionContainer: {
+  mainContainer: {
     flexDirection: 'column',
-    marginLeft: 250, // Espacio para alinear las secciones a la derecha del rectángulo
     padding: 30,
-    flexGrow: 1, // Para ocupar el espacio disponible a la derecha del rectángulo
+    flexGrow: 1,
   },
   section: {
     marginBottom: 10,
@@ -32,18 +32,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   value: {
-    fontSize: 12,
+    fontSize: 10,
   },
   footer: {
     position: 'absolute',
-    bottom: -10,
+    bottom: 0,
     left: 0,
     right: 0,
     height: 100,
     width: '100%',
-    backgroundColor: 'pink',
-    margin: 0,
-    padding: 0,
   },
   footerImage: {
     width: '100%',
@@ -51,7 +48,7 @@ const styles = StyleSheet.create({
     objectFit: 'cover',
   },
   rectangle: {
-    position: 'absolute',
+    position: 'relative',
     top: 0,
     left: 0,
     width: 232,
@@ -68,35 +65,116 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.5,
     shadowRadius: 15,
     elevation: 10,
+    marginRight: 30,
+  },
+  projectName: {
+    marginBottom: 20,
+  },
+  projectDetails: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  projectDescription: {
+    flexGrow: 1,
+    marginRight: 20,
+  },
+  projectImageContainer: {
+    width: 220,
+    height: 367,
+    borderRadius: 10,
+    overflow: 'hidden',
+    shadowColor: '#000',
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: 0.5,
+    shadowRadius: 15,
+    elevation: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  projectImage: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+  rectangleContent: {
+    flexDirection: 'column',
+    padding: 10,
+  },
+  projectContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30,
+  },
+  textContainer: {
+    flex: 1,
+    paddingRight: 20,
+  },
+  imageContainer: {
+    width: 220,
+    height: 367,
+    borderRadius: 10,
+    overflow: 'hidden',
+    shadowColor: '#000',
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: 0.5,
+    shadowRadius: 15,
+    elevation: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
 const ProjectReport = ({ project }) => (
   <Document>
     <Page size="A4" style={styles.page} orientation="landscape">
-      <View style={styles.rectangle}></View>
-      <View style={styles.sectionContainer}>
-        <View style={styles.header}>
-          <Text style={styles.title}>Ficha del Proyecto</Text>
+      <View style={styles.rectangle}>
+        <View style={styles.rectangleContent}>
+          <View style={styles.section}>
+            <Text style={styles.label}>Inversión Estimada:</Text>
+            <Text style={styles.value}>{project.inversion_estimada}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.label}>Región:</Text>
+            <Text style={styles.value}>{project.region}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.label}>Municipio:</Text>
+            <Text style={styles.value}>{project.municipio}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.label}>Beneficiarios:</Text>
+            <Text style={styles.value}>{project.beneficiarios}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.label}>Sector:</Text>
+            <Text style={styles.value}>{project.sector}</Text>
+          </View>
         </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>ID del Proyecto:</Text>
-          <Text style={styles.value}>{project.project_id}</Text>
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>Descripción:</Text>
-          <Text style={styles.value}>{project.descripcion}</Text>
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>Situación Sin Proyecto:</Text>
-          <Text style={styles.value}>{project.situacion_sin_proyecto}</Text>
-        </View>
-        <View style={styles.section}>
-          <Text style={styles.label}>Objetivos:</Text>
-          <Text style={styles.value}>{project.objetivos}</Text>
-        </View>
-        {/* Añade más campos según sea necesario */}
       </View>
+
+      <View style={styles.mainContainer}>
+        <View style={styles.projectName}>
+          <Text style={styles.value}>{project.project_name}</Text>
+        </View>
+        <View style={styles.projectContainer}>
+          <View style={styles.textContainer}>
+            <View style={styles.section}>
+              <Text style={styles.label}>Descripción:</Text>
+              <Text style={styles.value}>{project.descripcion}</Text>
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.label}>Situación Sin Proyecto:</Text>
+              <Text style={styles.value}>{project.situacion_sin_proyecto}</Text>
+            </View>
+          </View>
+          <View style={styles.imageContainer}>
+            <Image src={proyectoImage} style={styles.projectImage} />
+          </View>
+        </View>
+      </View>
+
       <View style={styles.footer}>
         <Image src={footerImage} style={styles.footerImage} />
       </View>
