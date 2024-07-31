5d8f6b538eb796fc408c9ecff147ee38292b562b
gomez50057 <gomez.50057@gmail.com>
2024-07-31 16:40:28 -0600

    desinger dialog

--- src/pages/Responsible/ProjectDialog.js
+++ src/pages/Responsible/ProjectDialog.js
@@ -15,7 +15,6 @@ const ProjectDialog = ({ open, onClose, project, onChange, onSubmit, isEditMode 
   };
 
   const handleToggleBlockField = (key) => {
-    // Si el campo es 'project_id', 'fecha_registro' o 'user__username', no hacer nada
     if (key === 'project_id' || key === 'fecha_registro' || key === 'user__username') {
       return;
     }
@@ -40,6 +39,66 @@ const ProjectDialog = ({ open, onClose, project, onChange, onSubmit, isEditMode 
     }
   };
 
+  const renderInputField = (key) => (
+    <div key={key} className="dialog-input-container">
+      <label className="dialog-label">{key.replace('_', ' ')}</label>
+      <input
+        className="dialog-input"
+        type="text"
+        name={key}
+        value={project[key] || ''}
+        onChange={onChange}
+        disabled={project[`isBlocked_${key}`] || key === 'project_id' || key === 'fecha_registro' || key === 'user__username'}
+      />
+      {!(key === 'project_id' || key === 'fecha_registro' || key === 'user__username') && (
+        <span className="toggle-text" onClick={() => handleToggleBlockField(key)}>
+          {project[`isBlocked_${key}`] ? 'Desbloquear' : 'Bloquear'}
+        </span>
+      )}
+      {showObservationFields[key] ? (
+        <div className="dialog-observation-container">
+          <input
+            className="dialog-input"
+            type="text"
+            name={`observacion_${key}`}
+            placeholder="Agregar observación"
+            value={project[`observacion_${key}`] || ''}
+            onChange={onChange}
+          />
+          <span className="toggle-text" onClick={() => handleToggleObservationField(key)}>
+            Quitar comentario
+          </span>
+        </div>
+      ) : (
+        <span className="toggle-text" onClick={() => handleToggleObservationField(key)}>
+          Agregar observación
+        </span>
+      )}
+    </div>
+  );
+
+  const renderSelectField = (key, options) => (
+    <div key={key} className="dialog-select-container">
+      <label className="dialog-label">{key.replace('_', ' ')}</label>
+      <select
+        name={key}
+        value={project[key] || ''}
+        onChange={onChange}
+        className="dialog-select"
+        disabled={project[`isBlocked_${key}`]}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <span className="toggle-text" onClick={() => handleToggleBlockField(key)}>
+        {project[`isBlocked_${key}`] ? 'Desbloquear' : 'Bloquear'}
+      </span>
+    </div>
+  );
+
   return (
     <div className="dialog-overlay">
       <div className="dialog">
@@ -47,100 +106,107 @@ const ProjectDialog = ({ open, onClose, project, onChange, onSubmit, isEditMode 
           {isEditMode ? 'Editar Proyecto' : 'Agregar Proyecto'}
         </div>
         <div className="dialog-content">
-          {Object.keys(project).map((key) => (
-            key !== 'id' && // No mostrar el campo 'id'
-            key !== 'estatus' &&
-            key !== 'situacion' &&
-            !key.startsWith('isBlocked_') &&
-            !key.startsWith('observacion_') &&
-            key !== 'retroalimentacion' ? (
-              <div key={key} className="dialog-input-container">
-                <label className="dialog-label">{key.replace('_', ' ')}</label>
-                <input
-                  className="dialog-input"
-                  type="text"
-                  name={key}
-                  value={project[key] || ''}
-                  onChange={onChange}
-                  disabled={project[`isBlocked_${key}`] || key === 'project_id' || key === 'fecha_registro' || key === 'user__username'} // Bloquear el campo si está marcado como bloqueado o es uno de los campos inmutables
-                />
-                {!(key === 'project_id' || key === 'fecha_registro' || key === 'user__username') && (
-                  <span className="toggle-text" onClick={() => handleToggleBlockField(key)}>
-                    {project[`isBlocked_${key}`] ? 'Desbloquear' : 'Bloquear'}
-                  </span>
-                )}
-                {showObservationFields[key] ? (
-                  <div className="dialog-observation-container">
-                    <input
-                      className="dialog-input"
-                      type="text"
-                      name={`observacion_${key}`}
-                      placeholder="Agregar observación"
-                      value={project[`observacion_${key}`] || ''}
-                      onChange={onChange}
-                    />
-                    <span className="toggle-text" onClick={() => handleToggleObservationField(key)}>
-                      Quitar comentario
-                    </span>
-                  </div>
-                ) : (
-                  <span className="toggle-text" onClick={() => handleToggleObservationField(key)}>
-                    Agregar observación
-                  </span>
-                )}
-              </div>
-            ) : (
-              key === 'estatus' ? (
-                <div key={key} className="dialog-select-container">
-                  <label className="dialog-label">Estatus</label>
-                  <select
-                    name="estatus"
-                    value={project.estatus || ''}
-                    onChange={onChange}
-                    className="dialog-select"
-                    disabled={project[`isBlocked_estatus`]} // Bloquear el campo si está marcado como bloqueado
-                  >
-                    <option value="Atendido">Atendido</option>
-                    <option value="En Proceso">En Proceso</option>
-                    <option value="Sin Avance">Sin Avance</option>
-                  </select>
-                  <span className="toggle-text" onClick={() => handleToggleBlockField('estatus')}>
-                    {project[`isBlocked_estatus`] ? 'Desbloquear' : 'Bloquear'}
-                  </span>
-                </div>
-              ) : (
-                key === 'situacion' && (
-                  <div key={key} className="dialog-select-container">
-                    <label className="dialog-label">Situación</label>
-                    <select
-                      name="situacion"
-                      value={project.situacion || ''}
-                      onChange={onChange}
-                      className="dialog-select"
-                      disabled={project[`isBlocked_situacion`]} // Bloquear el campo si está marcado como bloqueado
-                    >
-                      <option value="Vigente">Vigente</option>
-                      <option value="Antecedente">Antecedente</option>
-                      <option value="Cancelado">Cancelado</option>
-                    </select>
-                    <span className="toggle-text" onClick={() => handleToggleBlockField('situacion')}>
-                      {project[`isBlocked_situacion`] ? 'Desbloquear' : 'Bloquear'}
-                    </span>
-                  </div>
-                )
-              )
-            )
-          ))}
-          <div className="dialog-input-container">
-            <label className="dialog-label">Retroalimentación</label>
-            <input
-              className="dialog-input"
-              type="text"
-              name="retroalimentacion"
-              value={project.retroalimentacion || ''}
-              onChange={onChange}
-            />
+          <div className="dialog-row">
+            {renderInputField('project_id')}
+            {renderInputField('fecha_registro')}
           </div>
+          {renderInputField('project_name')}
+          <div className="dialog-row">
+            {renderInputField('sector')}
+            {renderInputField('tipo_proyecto')}
+            {renderInputField('tipo_entidad')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('dependencia')}
+            {renderInputField('organismo')}
+            {renderInputField('municipioEnd')}
+            {renderInputField('peticion_personal')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('unidad_responsable')}
+            {renderInputField('unidad_presupuestal')}
+            {renderInputField('ramo_presupuestal')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('monto_federal')}
+            {renderInputField('monto_estatal')}
+            {renderInputField('monto_municipal')}
+            {renderInputField('monto_otros')}
+          </div>
+          {renderInputField('inversion_estimada')}
+          {renderInputField('descripcion')}
+          {renderInputField('situacion_sin_proyecto')}
+          <div className="dialog-row">
+            {renderInputField('objetivos')}
+            {renderInputField('metas')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('gasto_programable')}
+            {renderInputField('programa_presupuestario')}
+            {renderInputField('beneficiarios')}
+          </div>
+          {renderInputField('alineacion_normativa')}
+          <div className="dialog-row">
+            {renderInputField('region')}
+            {renderInputField('municipio')}
+            {renderInputField('municipio_impacto')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('localidad')}
+            {renderInputField('barrio_colonia_ejido')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('latitud')}
+            {renderInputField('longitud')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('plan_nacional')}
+            {renderInputField('plan_estatal')}
+            {renderInputField('plan_municipal')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('ods')}
+            {renderInputField('plan_sectorial')}
+            {renderInputField('indicadores_estrategicos')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('indicadores_tacticos')}
+            {renderInputField('indicadores_desempeno')}
+            {renderInputField('indicadores_rentabilidad')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('estado_inicial')}
+            {renderInputField('estado_con_proyecto')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('estudios_prospectivos')}
+            {renderInputField('estudios_factibilidad')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('analisis_alternativas')}
+            {renderInputField('validacion_normativa')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('liberacion_derecho_via')}
+            {renderInputField('situacion_sin_proyecto_fotografico')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('situacion_con_proyecto_proyeccion')}
+            {renderInputField('analisis_costo_beneficio')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('expediente_tecnico')}
+            {renderInputField('proyecto_ejecutivo')}
+          </div>
+          <div className="dialog-row">
+            {renderInputField('manifestacion_impacto_ambiental')}
+            {renderInputField('otros_estudios')}
+          </div>
+          {renderInputField('observaciones')}
+          {renderInputField('porcentaje_avance')}
+          {renderSelectField('estatus', ['Atendido', 'En Proceso', 'Sin Avance'])}
+          {renderSelectField('situacion', ['Vigente', 'Antecedente', 'Cancelado'])}
+          {renderInputField('retroalimentacion')}
         </div>
         <div className="dialog-actions">
           <button className="dialog-button" onClick={onClose}>Cancelar</button>
