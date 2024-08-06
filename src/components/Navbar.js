bb672687c14b0c13b6efee44cf405c3a58b2fbab
gomez50057 <gomez.50057@gmail.com>
2024-08-05 22:15:59 -0600

    navbar_opc

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,12 +33,17 @@ const Navbar = () => {
     return (
         <nav className={`navbar ${visible ? 'active' : 'hidden'} ${scrollPosition > 100 ? 'scrolled' : ''}`}>
             <ul>
-                <img src={`${imgBasePath}Logotipo.webp`} alt="img_representativa" />
-                <li><a href="#about" onClick={(e) => handleScroll(e, 'about')}>Sobre el Proyecto</a></li>
-                <li><a href="#projects" onClick={(e) => handleScroll(e, 'projects')}>Tablero de Proyectos</a></li>
-                <li><a href="#howitWorks" onClick={(e) => handleScroll(e, 'howitWorks')}>Cómo Funciona</a></li>
-                {/* <li><a href="#map">Mapa Interactivo</a></li> */}
-                <li><Link to="/login" className="button_acceder">Acceder</Link></li>
+                <div className="navbar_logo">
+                    <img src={`${imgBasePath}Logotipo.webp`} alt="img_representativa" />
+                </div>
+                <div className="navbar_opc">
+                    <li><a href="#about" onClick={(e) => handleScroll(e, 'about')}>Sobre el Proyecto</a></li>
+                    <li><a href="#projects" onClick={(e) => handleScroll(e, 'projects')}>Tablero de Proyectos</a></li>
+                    <li><a href="#howitWorks" onClick={(e) => handleScroll(e, 'howitWorks')}>Cómo Funciona</a></li>
+                    {/* <li><a href="#map">Mapa Interactivo</a></li> */}
+                    <li><Link to="/login" className="button_acceder">Acceder</Link></li>
+                </div>
+
             </ul>
         </nav>
     );
