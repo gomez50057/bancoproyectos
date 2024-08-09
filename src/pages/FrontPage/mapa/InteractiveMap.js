ada9366e05d5f1599f0076450cb9c9d3218cc178
gomez50057 <gomez.50057@gmail.com>
2024-08-09 13:42:34 -0600

    InteractiveMap optimizacion

--- src/pages/FrontPage/mapa/InteractiveMap.js
+++ src/pages/FrontPage/mapa/InteractiveMap.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster';
@@ -20,9 +20,10 @@ const sectorIcons = {
 
 const InteractiveMap = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [selectedSector, setSelectedSector] = useState(''); // Estado para el sector seleccionado
+    const [selectedSector, setSelectedSector] = useState('');
     const mapRef = useRef(null);
-    const markersRef = useRef(L.markerClusterGroup()); // Referencia para los marcadores
+    const markersRef = useRef(L.markerClusterGroup());
+    const [projects, setProjects] = useState([]);
 
     useEffect(() => {
         // Inicializa el mapa
@@ -40,62 +41,37 @@ const InteractiveMap = () => {
             subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
         }).addTo(mapRef.current);
 
-        mapRef.current.attributionControl.setPrefix(''); // Elimina cualquier texto de atribución
+        mapRef.current.attributionControl.setPrefix('');
 
-        // Estilo para las áreas geográficas del mapa
-        function style(feature) {
-            return {
+        // Añadir las áreas geográficas al mapa
+        const geojson = L.geoJson(municipios_proyectos, {
+            style: {
                 fillColor: 'rgba(0, 0, 0, 0.4)',
                 weight: 1,
                 opacity: 0.7,
                 color: '#fff',
                 fillOpacity: 0.6
-            };
-        }
-
-        // Añadiendo las áreas geográficas al mapa
-        const geojson = L.geoJson(municipios_proyectos, {
-            style: style,
+            },
         }).addTo(mapRef.current);
 
-        // Funciones para resaltar y ajustar municipios seleccionados
-        function resetMunicipiosStyle() {
+        document.getElementById('selectorMunicipio').addEventListener('change', function () {
+            const selectedMunicipio = this.value;
             geojson.eachLayer(function (layer) {
                 geojson.resetStyle(layer);
-            });
-        }
-
-        function highlightMunicipio(selectedMunicipio) {
-            let foundLayer = null;
-            geojson.eachLayer(function (layer) {
                 if (layer.feature.properties.NOMGEO === selectedMunicipio) {
-                    foundLayer = layer;
                     layer.setStyle({
                         color: '#691B32',
                         fillColor: '#691B32',
                         fillOpacity: 0.2,
                         weight: 5
                     });
+                    mapRef.current.fitBounds(layer.getBounds());
                 }
             });
-
-            if (foundLayer) {
-                mapRef.current.fitBounds(foundLayer.getBounds());
-            } else {
-                console.warn(`Municipio ${selectedMunicipio} no encontrado.`);
-            }
-        }
-
-        document.getElementById('selectorMunicipio').addEventListener('change', function () {
-            const selectedMunicipio = this.value;
-            resetMunicipiosStyle();
-            highlightMunicipio(selectedMunicipio);
         });
 
         // Rellenar selector de municipios con opciones
         const selectorMunicipio = document.getElementById('selectorMunicipio');
-
-        // Ordenar municipios alfabéticamente y añadir al selector
         municipios_proyectos.features
             .map(municipio => municipio.properties.NOMGEO)
             .sort()
@@ -109,56 +85,15 @@ const InteractiveMap = () => {
         // Ajusta el tamaño del mapa después de la transición
         setTimeout(() => mapRef.current.invalidateSize(), 300);
 
-        // Obtener los datos de los proyectos y agregar los marcadores con íconos personalizados
+        // Obtener los datos de los proyectos
         const fetchData = async () => {
             try {
                 const response = await axios.get('/proyecto/');
-                const projects = response.data.filter(project => project.estatus === 'Atendido');
-
-                if (projects.length === 0) {
+                const filteredProjects = response.data.filter(project => project.estatus === 'Atendido');
+                setProjects(filteredProjects);
+                if (filteredProjects.length === 0) {
                     alert('No se encontraron proyectos con estatus "Atendido".');
                 }
-
-                markersRef.current.clearLayers(); // Limpiar los marcadores actuales
-
-                projects.forEach(project => {
-                    if (project.latitud && project.longitud) {
-                        // Determinar el ícono basado en el sector del proyecto, o usar un ícono por defecto
-                        const iconUrl = sectorIcons[project.sector] || `${iconBaseUrl}default.png`;
-
-                        const customIcon = L.icon({
-                            iconUrl: iconUrl,
-                            iconSize: [60, 60], // Tamaño del icono
-                            iconAnchor: [40, 40], // Punto donde el ícono se ancla en las coordenadas
-                            popupAnchor: [0, -32] // Punto desde donde el popup se abre
-                        });
-
-                        const popupContent = `
-                        <div class="popup-content">
-                            <strong class="popup-project-name">${project.project_name}</strong><br>
-                            <span class="popup-label">Tipo de Proyecto:</span> ${project.tipo_proyecto}<br>
-                            <span class="popup-label">Sector:</span> ${project.sector}<br>
-                            <span class="popup-label">Municipio:</span> ${project.municipioEnd}<br>
-                            <span class="popup-label">Inversión Estimada:</span> ${project.inversion_estimada}<br>
-                            <span class="popup-label">Descripción:</span> ${project.descripcion}<br>
-                            <span class="popup-label">Objetivos:</span> ${project.objetivos}<br>
-                            <span class="popup-label">Metas:</span> ${project.metas}<br>
-                            <span class="popup-label">Beneficiarios:</span> ${project.beneficiarios}<br>
-                            <span class="popup-label">Región:</span> ${project.region}<br>
-                            <span class="popup-label">Localidad:</span> ${project.localidad}<br>
-                            <span class="popup-label">Barrio/Colonia/Ejido:</span> ${project.barrio_colonia_ejido}<br>
-                            <span class="popup-label">Estatus:</span> ${project.estatus}
-                        </div>
-                    `;
-
-                        const marker = L.marker([project.latitud, project.longitud], { icon: customIcon })
-                            .bindPopup(popupContent);
-
-                        markersRef.current.addLayer(marker);
-                    }
-                });
-
-                mapRef.current.addLayer(markersRef.current);
             } catch (error) {
                 console.error('Error fetching project data:', error);
             }
@@ -171,59 +106,51 @@ const InteractiveMap = () => {
         };
     }, []);
 
-    // Filtrar marcadores por sector
-    useEffect(() => {
-        if (markersRef.current) {
-            markersRef.current.clearLayers(); // Limpiar los marcadores actuales
+    const addMarkers = useCallback(() => {
+        markersRef.current.clearLayers();
+        projects
+            .filter(project => !selectedSector || project.sector === selectedSector)
+            .forEach(project => {
+                if (project.latitud && project.longitud) {
+                    const iconUrl = sectorIcons[project.sector] || `${iconBaseUrl}default.png`;
 
-            axios.get('/proyecto/').then(response => {
-                const projects = response.data.filter(project =>
-                    project.estatus === 'Atendido' &&
-                    (selectedSector === '' || project.sector === selectedSector)
-                );
+                    const customIcon = L.icon({
+                        iconUrl: iconUrl,
+                        iconSize: [60, 60],
+                        iconAnchor: [40, 40],
+                        popupAnchor: [0, -32]
+                    });
 
-                projects.forEach(project => {
-                    if (project.latitud && project.longitud) {
-                        const iconUrl = sectorIcons[project.sector] || `${iconBaseUrl}default.png`;
+                    const popupContent = `
+                    <div class="popup-content">
+                        <strong class="popup-project-name">${project.project_name}</strong><br>
+                        <span class="popup-label">Tipo de Proyecto:</span> ${project.tipo_proyecto}<br>
+                        <span class="popup-label">Sector:</span> ${project.sector}<br>
+                        <span class="popup-label">Municipio:</span> ${project.municipioEnd}<br>
+                        <span class="popup-label">Descripción:</span> ${project.descripcion}<br>
+                        <span class="popup-label">Objetivos:</span> ${project.objetivos}<br>
+                        <span class="popup-label">Metas:</span> ${project.metas}<br>
+                        <span class="popup-label">Beneficiarios:</span> ${project.beneficiarios}<br>
+                        <span class="popup-label">Región:</span> ${project.region}<br>
+                        <span class="popup-label">Localidad:</span> ${project.localidad}<br>
+                        <span class="popup-label">Barrio/Colonia/Ejido:</span> ${project.barrio_colonia_ejido}<br>
+                    </div>
+                    `;
 
-                        const customIcon = L.icon({
-                            iconUrl: iconUrl,
-                            iconSize: [60, 60],
-                            iconAnchor: [40, 40],
-                            popupAnchor: [0, -32]
-                        });
+                    const marker = L.marker([project.latitud, project.longitud], { icon: customIcon })
+                        .bindPopup(popupContent);
 
-                        const popupContent = `
-    <div class="popup-content">
-        <strong class="popup-project-name">${project.project_name}</strong><br>
-        <span class="popup-label">Tipo de Proyecto:</span> ${project.tipo_proyecto}<br>
-        <span class="popup-label">Sector:</span> ${project.sector}<br>
-        <span class="popup-label">Municipio:</span> ${project.municipioEnd}<br>
-        <span class="popup-label">Inversión Estimada:</span> ${project.inversion_estimada}<br>
-        <span class="popup-label">Descripción:</span> ${project.descripcion}<br>
-        <span class="popup-label">Objetivos:</span> ${project.objetivos}<br>
-        <span class="popup-label">Metas:</span> ${project.metas}<br>
-        <span class="popup-label">Beneficiarios:</span> ${project.beneficiarios}<br>
-        <span class="popup-label">Región:</span> ${project.region}<br>
-        <span class="popup-label">Localidad:</span> ${project.localidad}<br>
-        <span class="popup-label">Barrio/Colonia/Ejido:</span> ${project.barrio_colonia_ejido}<br>
-        <span class="popup-label">Estatus:</span> ${project.estatus}
-    </div>
-`;
-
-                        const marker = L.marker([project.latitud, project.longitud], { icon: customIcon })
-                            .bindPopup(popupContent);
-
-                        markersRef.current.addLayer(marker);
-                    }
-                });
-
-                mapRef.current.addLayer(markersRef.current);
-            }).catch(error => {
-                console.error('Error fetching filtered project data:', error);
+                    markersRef.current.addLayer(marker);
+                }
             });
+        mapRef.current.addLayer(markersRef.current);
+    }, [projects, selectedSector]);
+
+    useEffect(() => {
+        if (projects.length > 0) {
+            addMarkers();
         }
-    }, [selectedSector]);
+    }, [projects, selectedSector, addMarkers]);
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
