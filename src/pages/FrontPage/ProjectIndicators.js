d46a559af7c14415446929698bfc42f81705ed8a
gomez50057 <gomez.50057@gmail.com>
2024-08-22 12:12:30 -0600

    dep y org suma

--- src/pages/FrontPage/ProjectIndicators.js
+++ src/pages/FrontPage/ProjectIndicators.js
@@ -7,9 +7,8 @@ const imgBasePath = "https://bibliotecadigitaluplaph.hidalgo.gob.mx/img_banco/in
 const ProjectIndicators = () => {
   const [counts, setCounts] = useState({
     citizens: 0,
-    departments: 0,
     municipalities: 0,
-    organizations: 0,
+    organizations: 0, // Aquí se almacenará la suma de organizaciones y departamentos
   });
   const [hasCounted, setHasCounted] = useState(false);
   const indicatorsRef = useRef(null);
@@ -45,9 +44,10 @@ const ProjectIndicators = () => {
           const projects = response.data;
           const totalValues = {
             citizens: projects.filter(project => project.tipo_entidad === 'Petición Personal').length,
-            departments: projects.filter(project => project.tipo_entidad === 'Dependencia').length,
             municipalities: projects.filter(project => project.tipo_entidad === 'Municipio').length,
-            organizations: projects.filter(project => project.tipo_entidad === 'Organismo').length,
+            organizations: projects.filter(project => 
+              project.tipo_entidad === 'Organismo' || project.tipo_entidad === 'Dependencia'
+            ).length,
           };
 
           const fastSpeed = 10;
@@ -85,9 +85,8 @@ const ProjectIndicators = () => {
     <section id='projects' className="ProjectIndicators-container" ref={indicatorsRef}>
       <div className="indicators">
         <Indicator count={counts.citizens} imgSrc={`${imgBasePath}Ciudadania.webp`} label="Ciudadanía" />
-        <Indicator count={counts.departments} imgSrc={`${imgBasePath}Dependencia.webp`} label="Dependencia" />
         <Indicator count={counts.municipalities} imgSrc={`${imgBasePath}Municipio.webp`} label="Municipio" />
-        <Indicator count={counts.organizations} imgSrc={`${imgBasePath}Organismo.webp`} label="Organismo" />
+        <Indicator count={counts.organizations} imgSrc={`${imgBasePath}Organismo.webp`} label="Dependencia y Organismo" /> 
       </div>
     </section>
   );
