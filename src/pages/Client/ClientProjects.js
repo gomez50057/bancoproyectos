e58cc48ecbd7ce7afc383e49706f71bfd4bfae36
gomez50057 <gomez.50057@gmail.com>
2024-08-23 18:27:00 -0600

    tabla

--- src/pages/Client/ClientProjects.js
+++ src/pages/Client/ClientProjects.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import MUIDataTable from 'mui-datatables';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, Typography } from '@mui/material';
+import { CssBaseline, Typography, Tooltip } from '@mui/material';  // Importar Tooltip
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -18,7 +18,8 @@ const ClientProjects = () => {
           project.project_id,
           project.project_name,
           project.estatus,
-          project.porcentaje_avance
+          project.porcentaje_avance,
+          project.isBlocked_project, // Asegúrate de que esta línea esté presente
         ]);
         setProjects(data);
       } catch (error) {
@@ -43,11 +44,76 @@ const ClientProjects = () => {
         setCellProps: () => ({ style: { textAlign: 'center' } }),
         customBodyRender: (value, tableMeta) => {
           const projectId = projects[tableMeta.rowIndex][0];
-          return (
-            <button className="crud-button" onClick={() => handleEditClick(projectId)}>
-              Editar
-            </button>
-          );
+          const estatus = projects[tableMeta.rowIndex][2];
+          const isBlocked = projects[tableMeta.rowIndex][4]; 
+
+          if (isBlocked) {
+            if (estatus === "Atendido") {
+              return (
+                <Tooltip title="Aceptado">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <path
+                      d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </Tooltip>
+              );
+            } else if (estatus === "Registrado") {
+              return (
+                <Tooltip title="Pronto estará en Revisión">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <path
+                      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </Tooltip>
+              );
+            } else if (estatus === "En Proceso") {
+              return (
+                <Tooltip title="Revisión">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <path
+                      d="M11 2v20c-1.1 0-2-.9-2-2H5c0 1.1-.9 2-2 2V2c1.1 0 2-.9 2-2h4c0 1.1.9 2 2 2zm2 0h4c1.1 0 2 .9 2 2v16c0 1.1-.9 2-2 2h-4V2z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </Tooltip>
+              );
+            }
+          } else {
+            // Mostrar botón "Editar" si el proyecto no está bloqueado
+            return (
+              <button 
+                className="crud-button" 
+                onClick={() => handleEditClick(projectId)} 
+              >
+                Editar
+              </button>
+            );
+          }
         }
       } 
     }
